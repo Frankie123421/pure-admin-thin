52892c44b2d3f7a6a63d290555b47addfc9cf74e
Frankie Lee <frankie@FrankiedeMacBook-Air.local>
2024-10-14 21:41:20 +0800

    feat: add new feature

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 import { getCaptcha } from "@/api/user";
+import { watchEffect } from "vue";
+import { useWebSocket } from "@vueuse/core";
+
+const { status, data, send, close, open } = useWebSocket(
+  "ws://127.0.0.1:3000/socket"
+);
+
+watchEffect(() => {
+  if (data) {
+    console.log(data.value);
+  }
+});
+
 defineOptions({
   name: "Welcome"
 });
-getCaptcha();
-getCaptcha();
+
 getCaptcha();
 </script>
 
 <template>
-  <h1>智慧AI实验室</h1>
+  <el-button @click="send('我是前端')">send</el-button>
 </template>
